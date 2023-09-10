d7cab8d071fcd9a4b38b2a695e9765b106e64eef
Nonoah001 <96695375+Nonoah001@users.noreply.github.com>
2023-09-10 18:41:04 +0200

    Added Main Script

--- Main.lua
+++ Main.lua
@@ -1,1 +1,14 @@
+function Tween(TI, prop, inst)
+  game:GetService("TweenService"):Create(inst, TI, prop):Play()
+end
 
+local w = workspace:GetDescendants()
+
+for i,v in pairs(w) do
+  if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("UnionOperation") then
+    v.Material = Enum.Material.Neon
+    v.Color = Color3.new(0,1,0)
+    Tween(v, TweenInfo.new(3, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut), {Transparency = 1})
+  end
+  wait(0.1)
+end
